0e5f247872deef31986527c484ce82f388377d48
Sean Yang <eiffel205566@hotmail.com>
2021-03-13 22:59:37 -0500

    2nd

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,24 @@
+import BlogLayout from 'src/layouts/BlogLayout/BlogLayout'
+
+const AboutPage = () => {
+  return <BlogLayout>This is About page</BlogLayout>
+}
+
+export default AboutPage
+
+// const AboutPage = () => {
+//   return (
+//     <>
+//       <h1>
+//         <Link to={routes.home()}>Redwood Blog</Link>
+//       </h1>
+//       <nav>
+//         <ul>
+//           <li>
+//             <Link to={routes.home()}>Home</Link>
+//           </li>
+//         </ul>
+//       </nav>
+//     </>
+//   )
+// }
---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,7 @@
+import BlogLayout from '../../layouts/BlogLayout'
+
+const HomePage = () => {
+  return <BlogLayout>This is home page</BlogLayout>
+}
+
+export default HomePage
