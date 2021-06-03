9b07e5793d460f941ede1a6106812d507bfe2e2f
Sean Yang <eiffel205566@hotmail.com>
2021-06-02 21:27:14 -0400

    update summary

--- web/src/components/Summary/Summary.js
+++ web/src/components/Summary/Summary.js
@@ -3,6 +3,7 @@ import * as _ from 'lodash'
 import { Wrapper } from 'src/components/Misc/UtilityFunc'
 import SingleType from 'src/components/DefaultType/SingleType'
 import { iconTypes, isTypeExpense } from '../DefaultType/Static'
+import { Money } from '../Misc/svg'
 
 const Summary = ({
   textExpenseByType,
@@ -14,14 +15,14 @@ const Summary = ({
 
   return (
     <Fragment>
-      <div className="textInsightSection text-white w-full max-h-24 border border-red-300 m-1">
+      <div className="textInsightSection text-white w-full max-h-24 border border-red-300 mb-1">
         <h1>xx</h1>
         <h1>xx</h1>
         <h1>xx</h1>
         <h1>xx</h1>
       </div>
       <section className="numberInsightsection text-displayOnly flex">
-        <div className="typeRank border border-yellow-300 max-h-96 flex-grow m-1">
+        <div className="typeRank border border-yellow-300 max-h-96 flex-grow">
           {textExpenseByType.map((oneType, index) => {
             const singleType = _.find(types, function (o) {
               return o.id === oneType.expenseTypeId
@@ -35,29 +36,43 @@ const Summary = ({
                 <Wrapper>
                   <SingleType
                     icon={
-                      singleType.description
-                        ? iconTypes[singleType.description]
+                      singleType?.description
+                        ? iconTypes[singleType?.description]
                         : iconTypes['CREDIT_CARD']
                     }
                     newName={
-                      singleType.newName ? singleType.newName : 'default'
+                      singleType?.newName ? singleType?.newName : 'default'
                     }
                     parentClass="w-16 sm:w-20"
                     iconClass="mx-auto h-6 w-6 text-displayOnly"
                     noHoverNeeded={true}
                     textColor="text-displayOnly"
-                    type={isTypeExpense(singleType.description) ? '' : 'income'}
+                    type={
+                      isTypeExpense(singleType?.description) ? '' : 'income'
+                    }
                   />
                 </Wrapper>
 
                 <Wrapper>
-                  <div>{` : $ ${oneType.sum.amount}`}</div>
+                  {/*
+                  <Money />
+                */}
+                  <div className="rank m-2 h-full w-80 border border-gray-500 ">
+                    <div className="rankBarContent w-20 h-full"></div>
+                  </div>
+                </Wrapper>
+                <Wrapper>
+                  <div>{` $ ${oneType.sum.amount}`}</div>
                 </Wrapper>
               </div>
             )
           })}
         </div>
-        <div className="typeRank border border-blue-300 h-20 flex-grow m-1"></div>
+        <div className="typeRank border border-blue-300 h-20 flex-grow">
+          <Wrapper>
+            <div className="rankBar m-2 h-full w-80 border border-green-500 "></div>
+          </Wrapper>
+        </div>
       </section>
     </Fragment>
   )
